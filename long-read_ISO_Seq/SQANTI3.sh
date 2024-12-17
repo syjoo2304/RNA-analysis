402790be4bc5cb8cff722933d66e831be1f3d026
syjoo2304 <46588210+syjoo2304@users.noreply.github.com>
2024-12-17 17:30:25 +0900

    Update SQANTI3.sh

--- long-read_ISO_Seq/SQANTI3.sh
+++ long-read_ISO_Seq/SQANTI3.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-sqantdir='/home/syjoo/Project/long-read/program/SQANTI3'
-refdir_='/home/syjoo/Project/long-read/REF'
-workdir_='/home/syjoo/Project/long-read/working_dir'
+sqantdir='[path_to_sqanti3]'
+isoannotdir='[path_to_isoannotatlite]'
+refdir_='[path_to_reference]'
+workdir_='[working_dir]'
 sample_gtf='clustered_2_mc.gff'
 prefix='clustered_2_mc'
 python $sqantdir/sqanti3_qc.py $workdir_/$sample_gtf $refdir_/gencode.vM20.chr_patch_hapl_scaff.annotation.gtf \
 $refdir_/GRCm38.p6.genome.fa -o ${prefix} -d $workdir_/SQANTI3_QC_output_${prefix}  --cpus 4 --report html
 
+python3 $isoannotdir/IsoAnnotLite_SQ3.py ${prefix}.gtf ${prefix}_classification.txt ${prefix}_junctions.txt  \
+-gff3 ${refdir_}/Mus_musculus_GRCm38_Ensembl_86.gff3 -o ${prefix}_newGFF3 -stdout ${prefix}_statisticalResults -novel -nointronic -saveTranscriptIDs
 
 
